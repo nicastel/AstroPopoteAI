bd6bc25cee812b2160c06fb023c25ec246deb57d
nicastel <nic.castel@gmail.com>
2024-08-14 17:50:29 +0200

    Fix upscale via astrosleuth

--- app.py
+++ app.py
@@ -406,7 +406,7 @@ class App:
             imgcv2resized = cv2.resize(imagecv2in,(max(original_width//tile_size * tile_size, tile_size),max(original_height//tile_size * tile_size, tile_size)),interpolation=cv2.INTER_CUBIC)
 
             # Allocate an image to save the tiles
-            imgresult = cv2.copyMakeBorder(imgcv2resized,0,0,0,0,cv2.BORDER_REPLICATE)
+            imgresult = cv2.resize(imgcv2resized,(0, 0),fx=scale,fy=scale,interpolation=cv2.INTER_CUBIC)
 
             for i, tile in enumerate(tile_process(model, imgcv2resized, scale, tile_size, yield_extra_details=True)):
 
